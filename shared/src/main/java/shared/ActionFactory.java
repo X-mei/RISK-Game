d0a8fa31c127bcdbae5f3e9bdc8bb9179691206b
mg476 <mg476@duke.edu>
2021-03-12 16:29:19 +0800

    Action modified

--- shared/src/main/java/shared/ActionFactory.java
+++ shared/src/main/java/shared/ActionFactory.java
@@ -1,11 +1,11 @@
 package shared;
 
 public class ActionFactory {
-  public Action createAttack(String name, String s) {
+  public BasicAction createAttack(String name, String s) {
     return new Attack(name, s);
   }
 
-  public Action createMove(String name, String s) {
+  public BasicAction createMove(String name, String s) {
     return new Move(name, s);
   }
 }
--- shared/src/main/java/shared/Territory.java
+++ shared/src/main/java/shared/Territory.java
@@ -1,6 +1,5 @@
 package shared;
 
-import java.util.HashMap;
 import java.util.HashSet;
 
 /**
