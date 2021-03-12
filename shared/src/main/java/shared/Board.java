aefcd4eff539a1b5bdb9e1ace503ca948115acb9
Yuepeng Li <yl737@duke.edu>
2021-03-12 22:59:11 +0800

    add process single basic move and attack

--- shared/src/main/java/shared/Board.java
+++ shared/src/main/java/shared/Board.java
@@ -12,29 +12,33 @@ import java.net.*;
 
 public class Board {
   protected int playerNum;
+  protected int totalUnitsNum;
   protected HashMap<String, LinkedHashSet<Territory>> gameBoard;
   //final LinkedHashSet<Territory> allTerritories;
   protected MapFactory mapF; 
   protected LinkedHashMap<String, Territory> allTerritory; 
   //protected LinkedHashSet<>
   protected UnitsFactory UnitsF; 
-  protected HashMap<String, Function<Integer, Units>> unitsCreateFunction;
+  protected HashMap<String, Function<Integer, Soldiers>> unitsCreateFunction;
   private final RuleChecker moveRuleChecker;
   private final RuleChecker attackRuleChecker;
   private HashMap<String, Integer> tempCount;
+  private LinkedHashSet<String> UnitName;
 
   public Board(int num, MapFactory mapFac, UnitsFactory UnitsFac){
     this.playerNum = num;
+    this.totalUnitsNum = 20;
     this.mapF = mapFac;
     this.gameBoard = mapF.getMap(num);
     this.UnitsF = UnitsFac;
-    this.unitsCreateFunction = new HashMap<String, Function<Integer, Units>>();
+    this.unitsCreateFunction = new HashMap<String, Function<Integer, Soldiers>>();
     for(String s : gameBoard.keySet()){
       for(Territory t : gameBoard.get(s)){
         allTerritory.put(t.getTerritoryName(), t);
       }
     }
-    
+    this.UnitName = new LinkedHashSet<>();
+    UnitName.add("Basic Soldiers");
     this.attackRuleChecker = new OwnerChecker(new NeighborChecker(new UnitMovingChecker(null)));
     this.moveRuleChecker = new 
   }
@@ -59,11 +63,8 @@ public class Board {
     return unitSetupStrings;
   }
 
-  public void processUnitSetup(String playerName, int[] count){
-    int ind = 0;
-    for (Territory t : gameBoard.get(playerName)){
-      t.setUnits(new BasicSoldiers(count[ind++]));
-    }
+  public void setUpUnitsCreationMap(){
+    unitsCreateFunction.put("Basic Soldiers", (count) -> UnitsF.createBasicSoldiers(count));
   }
 
   public Territory getTerritory(String name){
@@ -83,10 +84,73 @@ public class Board {
   public void updateTempCount(String name, Integer cnt){
     tempCount.put(name, tempCount.get(name) - cnt);
   }
-  
-  public void setUpUnitsCreationMap(){
-    for()
+
+  /**
+   * Set all kinds of units on one territory 
+   * @param territoryName is the target territory
+   * @param count is a array contains all numbers of each kind of unit
+   */
+  public void singleTerrotoryUnitSetup(String territoryName, int[] count){
+    int ind = 0;
+    Territory t = allTerritory.get(territoryName);
+    for (String s : UnitName){
+      Soldiers u = unitsCreateFunction.get(s).apply(count[ind ++]); 
+      t.setUnits(u);
+    }
   }
+
+  public void processSingleBasicAction(BasicAction basicAct){
+    if(basicAct.getActionName() == "M"){
+      processSingleBasicMove(basicAct);
+    }
+  }
+
+  public void processSingleBasicMove(BasicAction basicAct){
+    String src = basicAct.getSource();
+    String dest = basicAct.getDestination();
+    Soldiers srcBasicSoldier = getSoldiers("Basic Soldiers", src);
+    Soldiers destBasicSoldier = getSoldiers("Basic Soldiers", dest);;
+    int count = basicAct.getCount();
+    srcBasicSoldier.updateCount(srcBasicSoldier.getCount() - count);
+    destBasicSoldier.updateCount(destBasicSoldier.getCount() - count);
+  }
+
+  public void processSingleBasicAttack(BasicAction basicAct){
+    String src = basicAct.getSource();
+    String dest = basicAct.getDestination();
+    Soldiers srcBasicSoldier = getSoldiers("Basic Soldiers", src);
+    Soldiers destBasicSoldier = getSoldiers("Basic Soldiers", dest);;
+    int srcBasicSoldierNum = srcBasicSoldier.getCount();
+    int destBasicSoldierNum = destBasicSoldier.getCount();
+    //int count = basicAct.getCount();
+    while(srcBasicSoldierNum > 0 && destBasicSoldierNum > 0){
+      int srcRandom = srcBasicSoldier.randomNum();
+      int destRandom = destBasicSoldier.randomNum();
+      if(srcRandom > destRandom){
+        destBasicSoldier.updateCount(-1);
+      }
+      else if(srcRandom < destRandom){
+        srcBasicSoldier.updateCount(-1);
+      }
+      else{
+        continue;
+      }
+    }
+  }
+
+  private Soldiers getSoldiers(String UnitsName, String territoryName){
+    Territory terr = allTerritory.get(territoryName);
+    LinkedHashSet<Soldiers> terrAllUnits = terr.getUnits();
+    Soldiers terrBasicSoldier = null;
+    for(Soldiers u : terrAllUnits){
+      if(u.getName().equals("Basic Soldiers")){
+        terrBasicSoldier = u;
+        break;
+      }
+    }
+    return terrBasicSoldier;
+  }
+
 }
 
 
