21423f7eaa3a5bde44cff22a458fa95ee927663c
mg476 <mg476@duke.edu>
2021-03-13 23:30:52 +0800

    Neighbor Checker

--- shared/src/test/java/shared/NeighborCheckerTest.java
+++ shared/src/test/java/shared/NeighborCheckerTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-public class RouteCheckerTest {
+public class NeighborCheckerTest {
   @Test
-  public void test_RouteRule() {
+  public void test_CheckNeighbor() {
     Board b = new Board(2, new MapFactory(), new UnitsFactory());
     RuleChecker c = new NeighborChecker(null);
     BasicAction b1 = new Attack("King", "Dorado B 10");
@@ -14,6 +14,12 @@ public class RouteCheckerTest {
     BasicAction b3 = new Attack("King", "Hanamura Ilios 9");
     BasicAction b4 = new Attack("King", "Hanamura Junkertown 5");
     BasicAction b5 = new Attack("King", "A Junkertown 5");
+    assertEquals("The selected destination do not exist.", c.checkMyRule(b1, b));
+    assertEquals("Destination is not the nerghbour of the source!", c.checkMyRule(b2, b));
+    assertEquals(null, c.checkMyRule(b3, b));
+    assertEquals("Destination is not the nerghbour of the source!", c.checkMyRule(b4, b));
+    assertEquals("The selected source do not exist.", c.checkMyRule(b5, b));
+    
     
   }
 
