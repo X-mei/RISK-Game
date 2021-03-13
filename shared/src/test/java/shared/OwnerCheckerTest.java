1f7b03fda112cb89ad55bb6963ec7d4563773324
mg476 <mg476@duke.edu>
2021-03-13 23:02:10 +0800

    Checker

--- shared/src/test/java/shared/OwnerCheckerTest.java
+++ shared/src/test/java/shared/OwnerCheckerTest.java
@@ -1,16 +1,35 @@
 package shared;
 
 import java.io.IOException;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class OwnerCheckerTest {
   
   @Test
   public void test_OwnerRule() throws IOException {
-    ActionFactory f = new ActionFactory();
-    Player player = new Player("player1", 1, f);
-    BasicAction b1 = player.formAction("M", "A B 10");
+    BasicAction b1 = new Move("King", "Dorado B 10");
+    BasicAction b2 = new Move("King", "Hanamura B 10");
+    BasicAction b4 = new Attack("King", "Ilios B 10");
+    BasicAction b3 = new Attack("Red", "Ilios B 10");
+    BasicAction b5 = new Attack("Red", "A B 10");
+    Board b = new Board(2, new MapFactory(), new UnitsFactory());
+    OwnerChecker c = new OwnerChecker(null);
+    assertEquals("The selected source do not exist.", c.checkMyRule(b5, b));
+    assertEquals("You do not own the source territory.", c.checkMyRule(b4, b));
+    assertEquals(null, c.checkMyRule(b3, b));
+    assertEquals(null, c.checkMyRule(b2, b));
+    assertEquals(null, c.checkMyRule(b1, b));
   }
-
 }
+
+
+
+
+
+
+
+
+
+
+
---
+++ shared/src/test/java/shared/RouteCheckerTest.java
@@ -0,0 +1,13 @@
+package shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class RouteCheckerTest {
+  @Test
+  public void test_RouteRule() {
+    
+  }
+
+}
