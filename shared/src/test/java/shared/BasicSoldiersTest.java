afc3c4e370d52d2acdcca6d1b8cceb43e6d89d56
Yuepeng Li <yl737@duke.edu>
2021-03-30 23:53:03 +0800

    delete old tests

--- shared/src/test/java/shared/BasicSoldiersTest.java
+++ shared/src/test/java/shared/BasicSoldiersTest.java
@@ -5,43 +5,43 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class BasicSoldiersTest {
-  @Test
-  public void test_getCount() {
-    UnitsFactory f = new UnitsFactory();
-    Units u = f.createLevel1Soldiers(10);
-    assertEquals(10, u.getCount());
-  }
+  // @Test
+  // public void test_getCount() {
+  //   UnitsFactory f = new UnitsFactory();
+  //   Units u = f.createLevel1Soldiers(10);
+  //   assertEquals(10, u.getCount());
+  // }
 
-  @Test
-  public void test_modifyCount() {
-    UnitsFactory f = new UnitsFactory();
-    Units u = f.createLevel1Soldiers(10);
-    u.updateCount(u.getCount()-5);
-    assertEquals(5, u.getCount());
-    u.updateCount(u.getCount()+2);
-    assertEquals(7, u.getCount());
-  }
+  // @Test
+  // public void test_modifyCount() {
+  //   UnitsFactory f = new UnitsFactory();
+  //   Units u = f.createLevel1Soldiers(10);
+  //   u.updateCount(u.getCount()-5);
+  //   assertEquals(5, u.getCount());
+  //   u.updateCount(u.getCount()+2);
+  //   assertEquals(7, u.getCount());
+  // }
 
-  @Test
-  public void test_getName() {
-    UnitsFactory f = new UnitsFactory();
-    Units u = f.createLevel1Soldiers(10);
-    assertEquals("Basic Soldiers", u.getName());
-  }
+  // @Test
+  // public void test_getName() {
+  //   UnitsFactory f = new UnitsFactory();
+  //   Units u = f.createLevel1Soldiers(10);
+  //   assertEquals("Basic Soldiers", u.getName());
+  // }
 
-  @Test
-  public void test_random() {
-    Level1Soldiers b = new Level1Soldiers(10);
-    assert(b.randomNum() >= 1 && b.randomNum() <= 20);
-  }
+  // @Test
+  // public void test_random() {
+  //   Level1Soldiers b = new Level1Soldiers(10);
+  //   assert(b.randomNum() >= 1 && b.randomNum() <= 20);
+  // }
 
-  @Test
-  public void test_updateCount() {
-    Soldiers s = new Level1Soldiers(6);
-    assertEquals(6, s.getCount());
-    s.updateCount(8);
-    assertEquals(8, s.getCount());
-  }
+  // @Test
+  // public void test_updateCount() {
+  //   Soldiers s = new Level1Soldiers(6);
+  //   assertEquals(6, s.getCount());
+  //   s.updateCount(8);
+  //   assertEquals(8, s.getCount());
+  // }
 
 }
 
--- shared/src/test/java/shared/UnitMovingCheckerTest.java
+++ shared/src/test/java/shared/UnitMovingCheckerTest.java
@@ -5,29 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class UnitMovingCheckerTest {
-  @Test
-  public void test_CheckUnitMoving() {
-    Board b = new Board(2, new MapFactory(), new UnitsFactory());
-    RuleChecker c = new UnitMovingChecker(null);  
-    b.singleTerritoryUnitSetup("Dorado", new int[]{5});
-    b.singleTerritoryUnitSetup("Hanamura", new int[]{10});
-    b.singleTerritoryUnitSetup("Hollywood", new int[]{5});
-    b.singleTerritoryUnitSetup("Volskaya", new int[]{5});
-    b.singleTerritoryUnitSetup("Ilios", new int[]{10});
-    b.singleTerritoryUnitSetup("Junkertown", new int[]{5});
-    BasicAction b1 = new Move("King", "Dorado B 10");
-    BasicAction b2 = new Move("King", "Hanamura Hollywood 5");
-    BasicAction b3 = new Attack("King", "Hanamura B 9");
-    BasicAction b4 = new Move("King", "A B 5");
-    b.refreshTemp("King");
-    b.refreshTemp("Red");
-    assertEquals("The selected source do not exist.", c.checkMyRule(b4, b));
-    assertEquals("You do not have enough unit to move.", c.checkMyRule(b1, b));
-    b.refreshTemp("King");
-    b.refreshTemp("Red");
-    assertEquals(null, c.checkMyRule(b2, b));
-    assertEquals("You do not have enough unit to move.", c.checkMyRule(b3, b));
-  }
+  // @Test
+  // public void test_CheckUnitMoving() {
+  //   Board b = new Board(2, new MapFactory(), new UnitsFactory());
+  //   RuleChecker c = new UnitMovingChecker(null);  
+  //   b.singleTerritoryUnitSetup("Dorado", new int[]{5});
+  //   b.singleTerritoryUnitSetup("Hanamura", new int[]{10});
+  //   b.singleTerritoryUnitSetup("Hollywood", new int[]{5});
+  //   b.singleTerritoryUnitSetup("Volskaya", new int[]{5});
+  //   b.singleTerritoryUnitSetup("Ilios", new int[]{10});
+  //   b.singleTerritoryUnitSetup("Junkertown", new int[]{5});
+  //   BasicAction b1 = new Move("King", "Dorado B 10");
+  //   BasicAction b2 = new Move("King", "Hanamura Hollywood 5");
+  //   BasicAction b3 = new Attack("King", "Hanamura B 9");
+  //   BasicAction b4 = new Move("King", "A B 5");
+  //   b.refreshTemp("King");
+  //   b.refreshTemp("Red");
+  //   assertEquals("The selected source do not exist.", c.checkMyRule(b4, b));
+  //   assertEquals("You do not have enough unit to move.", c.checkMyRule(b1, b));
+  //   b.refreshTemp("King");
+  //   b.refreshTemp("Red");
+  //   assertEquals(null, c.checkMyRule(b2, b));
+  //   assertEquals("You do not have enough unit to move.", c.checkMyRule(b3, b));
+  // }
 
 }
 
