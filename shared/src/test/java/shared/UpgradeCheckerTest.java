c3de0b764481a5471f2fb00f7f4f383e59fdd607
mg476 <mg476@duke.edu>
2021-03-31 14:11:53 +0800

    checker test mod

---
+++ shared/src/test/java/shared/UpgradeCheckerTest.java
@@ -0,0 +1,13 @@
+package shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class UpgradeCheckerTest {
+  @Test
+  public void test_UpgradeAction() {
+    
+  }
+
+}
