cc378d637c27026b9fa2da3dc3a26e7125e8a01a
X-mei <gemg@mail.uc.edu>
2021-04-11 23:28:09 +0800

    Teleport unit added

--- shared/src/main/java/shared/TeleportSoldiers.java
+++ shared/src/main/java/shared/TeleportSoldiers.java
@@ -2,6 +2,6 @@ package shared;
 
 public class TeleportSoldiers extends Soldiers{
     public TeleportSoldiers(int count) {
-        super("Flying", count, 5, 55, 4);
+        super("Tel", count, 5, 55, 4);
     }
 }
