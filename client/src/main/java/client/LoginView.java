4620b3ed81bf7c8b2ea0c8f6ef016f070e70ed41
ys322 <ys322@duke.edu>
2021-04-14 00:06:18 +0800

    modify login window and enter game window

--- client/src/main/java/client/LoginView.java
+++ client/src/main/java/client/LoginView.java
@@ -5,6 +5,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+import java.io.FileInputStream;
+import java.io.IOError;
+import java.io.IOException;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class LoginView {
     AnchorPane loginPane;
@@ -28,50 +37,75 @@ public class LoginView {
     }
 
     public void init() {
+      try {
+        // add image
+        String path = "src/main/resources/riscTitle.png";
+        FileInputStream imageStream = new FileInputStream(path);
+        Image image = new Image(imageStream);
+        ImageView imageView = new ImageView(image);
+        imageView.setX(300);
+        imageView.setY(25);
+        imageView.setFitHeight(400);
+        imageView.setFitWidth(800);
+        loginPane.getChildren().add(imageView);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      
         // username Label
-        username.setLayoutX(20);
-        username.setLayoutY(75);
+        username.setLayoutX(550);
+        username.setLayoutY(400);
         username.setText("username: ");
         // username.setPrefSize(100, 50);
+        username.setFont(new Font("Arial", 15));
+        username.setTextFill(Color.web("#a0522d", 1.0));
         loginPane.getChildren().add(username);
 
         // username textField
-        text.setLayoutX(100);
-        text.setLayoutY(70);
+        text.setLayoutX(650);
+        text.setLayoutY(400);
         text.setPromptText("please input username");
         text.setFocusTraversable(false);
         loginPane.getChildren().add(text);
 
         // password label
-        pwd.setLayoutX(20);
-        pwd.setLayoutY(135);
+        pwd.setLayoutX(550);
+        pwd.setLayoutY(450);
         pwd.setText("password: ");
+        pwd.setFont(new Font("Arial", 15));
+        pwd.setTextFill(Color.web("#a0522d", 1.0));
         loginPane.getChildren().add(pwd);
 
         // password passwordField
-        password.setLayoutX(100);
-        password.setLayoutY(130);
+        password.setLayoutX(650);
+        password.setLayoutY(450);
         loginPane.getChildren().add(password);
 
         // button register
-        register.setLayoutX(100);
-        register.setLayoutY(200);
+        register.setLayoutX(600);
+        register.setLayoutY(550);
         register.setText("register");
         register.setId("register");
+        register.setStyle("-fx-background-color: #8FBC8F;");
+        register.setPrefWidth(80);
         loginPane.getChildren().add(register);
         
 
         // button login
-        login.setLayoutX(200);
-        login.setLayoutY(200);
+        login.setLayoutX(700);
+        login.setLayoutY(550);
         login.setText("login");
         login.setId("login");
+        login.setStyle("-fx-background-color: #CDCDB4;");
+        login.setPrefWidth(80);
         loginPane.getChildren().add(login);
 
         // error
-        error.setLayoutX(200);
-        error.setLayoutY(250);
+        error.setLayoutX(550);
+        error.setLayoutY(700);
         error.setText("Error! Please enter again.");
+        error.setFont(new Font("Arial", 20));
+        error.setTextFill(Color.web("#ff0000", 1.0));
         error.setVisible(false);
         loginPane.getChildren().add(error);
     }
