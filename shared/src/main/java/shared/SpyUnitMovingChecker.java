3ec7eed73cd28643a9b51d7b1b34fbb258f9b11d
X-mei <gemg@mail.uc.edu>
2021-04-13 15:39:03 +0800

    Spy refactored

---
+++ shared/src/main/java/shared/SpyUnitMovingChecker.java
@@ -0,0 +1,16 @@
+package shared;
+
+public class SpyUnitMovingChecker extends RuleChecker{
+    SpyUnitMovingChecker(RuleChecker next){
+        super(next);
+    }
+
+    @Override
+    protected String checkMyRule(BasicAction thisAct, Board theBoard){
+        int SpyCount = theBoard.getSpyCountByName(thisAct.getActionOwner(), thisAct.getSource());
+        if (SpyCount < thisAct.getCount()){
+            return "You do not have enough spy to move.";
+        }
+        return null;
+    }
+}
