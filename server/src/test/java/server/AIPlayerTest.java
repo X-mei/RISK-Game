d0ee8482141ce727e00121cea8b289debb3d9773
mg476 <mg476@duke.edu>
2021-04-14 23:34:38 +0800

    AI player test file create

---
+++ server/src/test/java/server/AIPlayerTest.java
@@ -0,0 +1,24 @@
+package server;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AIPlayerTest {
+  @Test
+  public void test_generateUpgrade() {
+    AIPlayer ai = new AIPlayer(null, null, null, 0, "Player 1");
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
