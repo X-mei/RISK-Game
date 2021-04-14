2d83754a86a142ddc88546c3103b3d688a5c21f0
X-mei <gemg@mail.uc.edu>
2021-04-14 23:41:26 +0800

    add testing file for AI player action generation

--- server/src/test/java/server/AIPlayerTest.java
+++ server/src/test/java/server/AIPlayerTest.java
@@ -3,11 +3,23 @@ package server;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import shared.Board;
+import shared.MapFactory;
+import shared.UnitsFactory;
 
 public class AIPlayerTest {
+  private Board getTestBoard() {
+    MapFactory f = new MapFactory();
+    UnitsFactory u = new UnitsFactory();
+    Board b = new Board(2, f, u);
+    return b;
+  }
+
   @Test
   public void test_generateUpgrade() {
-    AIPlayer ai = new AIPlayer(null, null, null, 0, "Player 1");
+    Board board = getTestBoard();
+    AIPlayer ai = new AIPlayer("a.b.c.d", 12345, null, 0, "Player 1");
+
   }
 
 }
