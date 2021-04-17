d1ca102b177eb92d04d15cdb16eae85ca532474e
ys322 <ys322@duke.edu>
2021-04-17 15:02:32 +0800

    modify enter game view

--- client/src/main/java/client/EnterGameView.java
+++ client/src/main/java/client/EnterGameView.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -16,19 +14,15 @@ import javafx.scene.text.Font;
 public class EnterGameView {
     AnchorPane enterGamePane;
     Label prompt;
-    RadioButton r1;
-    RadioButton r2;
-    ToggleGroup tg;
-    Button confirm;
+    Button previousGame;
+    Button newGame;
     Label error;
 
     public EnterGameView() {
         this.enterGamePane = new AnchorPane();
         this.prompt = new Label();
-        this.r1 = new RadioButton("Enter a Previous Game");
-        this.r2 = new RadioButton("Create a New Game");
-        this.tg = new ToggleGroup();
-        this.confirm = new Button();
+        this.previousGame = new Button();
+        this.newGame = new Button();
     }
 
     public void init() {
@@ -48,29 +42,27 @@ public class EnterGameView {
       }
 
         // prompt label
-        prompt.setLayoutX(500);
-        prompt.setLayoutY(400);
+        prompt.setLayoutX(480);
+        prompt.setLayoutY(350);
         prompt.setText("Do you want to enter a previous game or enter a new game?");
         prompt.setFont(new Font("Arial", 15));
         prompt.setTextFill(Color.web("#a0522d", 1.0));
         enterGamePane.getChildren().add(prompt);
 
-        // choice
-        r1.setToggleGroup(tg);
-        r1.setLayoutX(600);
-        r1.setLayoutY(500);
-        r2.setToggleGroup(tg);
-        r2.setLayoutX(600);
-        r2.setLayoutY(550);
-        enterGamePane.getChildren().add(r1);
-        enterGamePane.getChildren().add(r2);
+        // choice button
+        previousGame.setLayoutX(620);
+        previousGame.setLayoutY(400);
+        previousGame.setText("JOIN BEFORE!");
+        previousGame.setStyle("-fx-background-color: #8FBC8F;");
+        previousGame.setPrefSize(150, 80);
+        enterGamePane.getChildren().add(previousGame);
 
-        // confirm button
-        confirm.setLayoutX(650);
-        confirm.setLayoutY(600);
-        confirm.setText("confirm");
-        confirm.setStyle("-fx-background-color: #8FBC8F;");
-        enterGamePane.getChildren().add(confirm);
+        newGame.setLayoutX(620);
+        newGame.setLayoutY(500);
+        newGame.setText("CREATE NEW!");
+        newGame.setStyle("-fx-background-color: #8FBC8F;");
+        newGame.setPrefSize(150, 80);
+        enterGamePane.getChildren().add(newGame);
 
     }
     
